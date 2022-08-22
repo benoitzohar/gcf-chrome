ec2ecf854975ba351c1aa64b66847cbdb895e78f
Benoit Zohar <benoit.zohar@gmail.com>
2022-08-22 17:40:07 +0000

    yolo

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
-curl -o chrome.deb 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
-tar xf chrome.deb
+pwd
+curl --location -o chrome.tar.xz 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+file chrome.tar.xz
+ls -la
+tar xf chrome.tar.xz
 tar xf data.tar.xz
 rm data.tar.xz
 rm control.tar.xz
 rm -rf etc usr
-rm chrome.deb
+rm chrome.tar.xz
 rm debian-binary
