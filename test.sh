25540e15648f776f6dac2fb49a8c723fa21f133c
Benoit Zohar <benoit.zohar@gmail.com>
2022-08-22 17:55:35 +0000

    try something

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 pwd
-curl --location -o chrome.tar.xz 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
-file chrome.tar.xz
+curl --location -o chrome.deb 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+file chrome.deb
 ls -la
-tar xf chrome.tar.xz
+ar x chrome.deb
 tar xf data.tar.xz
 rm data.tar.xz
 rm control.tar.xz
 rm -rf etc usr
-rm chrome.tar.xz
+rm chrome.deb
 rm debian-binary
